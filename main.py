1c71fe2bc5ae5ee2b851af3c74e3c053fdae9999
codehia <dev@sacharya.dev>
2025-07-08 22:35:38 +0530

    Add helper functions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import csv
 from pprint import pp
+import copy
+
+
+# TODO: Consider this approach
+# {'1': (({R}, ...), ({C}, ...), ({B}, ...)), '2': {}, ...}
 
 """
 [['2', '1', '8', '9', '5', '4', '7', '6', '3'],
@@ -13,14 +17,65 @@ from pprint import pp
  ['6', '4', '5', '3', '1', '7', '9', '8', '2']]
 """
 
+"""
+ELEM: (i, j)
+ELEM: -> R, C, B
+
+while:
+    ({0..9} | candidates for (i, j)) - R - C - B
+    candidates -> len(candidates) == 1 -> final_value
+"""
+type Puzzle = list[list[str]]
+type ElementIndex = tuple[int, int]
+
+
+CANDIDATES = {
+    # (2, 3): {1, 2, 4}
+}
+
+EMPTY_VALUE_PLACEHOLDER = '0'
+BLOCK_SIZE = 3
+
+
+
+
+def get_column_values(element_index: ElementIndex, puzzle: list[list[str]]) -> set[str]:
+    # TODO: Consider pivot of the puzzle
+    column_values = set()
+    for row in puzzle:
+        for col_idx, val in enumerate(row):
+            if (col_idx == element_index[1]) and (val != EMPTY_VALUE_PLACEHOLDER):
+                column_values.add(val)
+    return column_values
+
+
+def get_row_values(element_index: ElementIndex, puzzle: list[list[str]]) -> set[str]:
+    return {elem for elem in puzzle[element_index[0]] if elem != EMPTY_VALUE_PLACEHOLDER}
+
+
+def get_block_values(element_index: ElementIndex, puzzle: list[list[str]]) -> set[str]:
+    row_start = element_index[0] // 3 * 3
+    col_start = element_index[1] // 3 * 3
+    return {
+        puzzle[row_idx][col_idx]
+        for row_idx in range(row_start, row_start + 3)
+        for col_idx in range(col_start, col_start + 3)
+        if puzzle[row_idx][col_idx] != EMPTY_VALUE_PLACEHOLDER
+    }
 
 def solve(puzzle: list[list[str]]) -> list[list[str]]:
     # Each list inside the outer list is a ROW (R)
-    # '0' is a empty value
     # Column (C)
     # Block (B)
-    pass
 
+    # for each [i][j] of the puzzle ->
+    # get_column_values
+    # get_row_values
+    # get_block_values
+    solution = copy.deepcopy(puzzle)
+
+    # TODO: Replace
+    return [[str(i) for i in range(1, 10)] for _ in range(9)]
 
 def main():
     ds = [['2', '1', '8', '9', '5', '4', '7', '6', '3'],
@@ -34,7 +89,6 @@ def main():
           ['6', '4', '5', '3', '1', '7', '9', '8', '2']]
     solution = solve(ds)
     pp(solution)
-
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import csv
 
 import pytest
 
-from main import solve
+from main import get_block_values, get_column_values, get_row_values, solve
 
 
 def parse_file(file_name):
@@ -13,10 +13,10 @@ def parse_file(file_name):
             solution = row["solutions"]
             chunk_size = 9
             quiz_fmt = [
-                list(quiz[i : i + chunk_size]) for i in range(0, len(quiz), chunk_size)
+                list(quiz[i: i + chunk_size]) for i in range(0, len(quiz), chunk_size)
             ]
             solution_fmt = [
-                list(solution[i : i + chunk_size])
+                list(solution[i: i + chunk_size])
                 for i in range(0, len(solution), chunk_size)
             ]
             yield quiz_fmt, solution_fmt
@@ -27,3 +27,58 @@ def test_solve(quiz, solution):
     assert solve(quiz) == solution, (
         f"Failed for quiz: {quiz} with expected solution: {solution}"
     )
+
+
+def test_get_column_values():
+    puzzle = [['2', '1', '8', '9', '5', '4', '7', '6', '3'],
+              ['7', '6', '3', '1', '2', '8', '5', '9', '4'],
+              ['5', '9', '4', '6', '7', '3', '2', '1', '8'],
+              ['4', '2', '9', '5', '3', '6', '8', '7', '1'],
+              ['3', '5', '6', '7', '8', '1', '0', '2', '9'],
+              ['8', '7', '1', '2', '4', '9', '3', '5', '6'],
+              ['9', '8', '2', '4', '6', '5', '1', '3', '7'],
+              ['1', '3', '7', '8', '9', '2', '6', '4', '5'],
+              ['6', '4', '5', '3', '1', '7', '9', '8', '2']]
+    idx_column_mapping = {
+        (0, 0): {'2', '7', '5', '4', '3', '8', '9', '1', '6'}
+    }
+    for idx, expected_col_values in idx_column_mapping.items():
+        column_values = get_column_values(idx, puzzle)
+        assert column_values == expected_col_values
+
+
+def test_get_row_values():
+    puzzle = [['2', '1', '8', '9', '5', '4', '7', '6', '3'],
+              ['7', '6', '3', '1', '2', '8', '5', '9', '4'],
+              ['5', '9', '4', '6', '7', '3', '2', '1', '8'],
+              ['4', '2', '9', '5', '3', '6', '8', '7', '1'],
+              ['3', '5', '6', '7', '8', '1', '0', '2', '9'],
+              ['8', '7', '1', '2', '4', '9', '3', '5', '6'],
+              ['9', '8', '2', '4', '6', '5', '1', '3', '7'],
+              ['1', '3', '7', '8', '9', '2', '6', '4', '5'],
+              ['6', '4', '5', '3', '1', '7', '9', '8', '2']]
+    idx_column_mapping = {
+        (0, 0): {'2', '1', '8', '9', '5', '4', '7', '6', '3'}
+    }
+    for idx, expected_col_values in idx_column_mapping.items():
+        column_values = get_row_values(idx, puzzle)
+        assert column_values == expected_col_values
+
+
+def test_get_block_values():
+    puzzle = [['2', '1', '8', '9', '5', '4', '7', '6', '3'],
+              ['7', '6', '3', '1', '2', '8', '5', '9', '4'],
+              ['5', '9', '4', '6', '7', '3', '2', '1', '8'],
+              ['4', '2', '9', '5', '3', '6', '8', '7', '1'],
+              ['3', '5', '6', '7', '8', '1', '0', '2', '9'],
+              ['8', '7', '1', '2', '4', '9', '3', '5', '6'],
+              ['9', '8', '2', '4', '6', '5', '1', '3', '7'],
+              ['1', '3', '7', '8', '9', '2', '6', '4', '5'],
+              ['6', '4', '5', '3', '1', '7', '9', '8', '2']]
+    idx_column_mapping = {
+        (7, 6): {'1', '3', '7', '6', '4', '5', '9', '8', '2'}
+    }
+
+    for idx, expected_col_values in idx_column_mapping.items():
+        column_values = get_block_values(idx, puzzle)
+        assert column_values == expected_col_values
